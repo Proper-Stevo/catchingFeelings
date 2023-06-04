4d415ae5d7f66b6d2d492be032543a12912a4ed8
Arely Rivera <arelyrivera1996@gmail.com>
2023-06-03 22:32:40 -0700

    added navigation container in app.js and moved homescreen to screens folder

--- App.js
+++ App.js
@@ -1,72 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
-// import AppNavigator from "./src/navigation/AppNavigator"
-import PlayholderWooper from "./assets/images/placeholder_wooper.png";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet } from "react-native";
 
-export default function App({ navigation }) {
-  const onPress = () => navigation.navigate('ArgHomeScreen');
+import HomeScreen from "./src/screens/HomeScreen.js"
+import ArgHomeScreen from "./src/screens/ArgHomeScreen.js"
 
+
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <NavigationContainer>
-      {/* <View style={styles.container}>
-        <Text style={styles.header}>Title TBD</Text>
-        <Image source={PlayholderWooper} style={styles.image} />
-        <Text style={styles.subheading}>Words Words Words</Text>
-        <Text style={styles.bodyText}>
-          Words Words Words Words Words Words Words Words Words Words Words
-        </Text>
-
-        <TouchableOpacity style={styles.button} onPress={onPress}>
-          <Text style={styles.buttonText}>Sign In</Text>
-        </TouchableOpacity>
-      </View> */}
+    <NavigationContainer >
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="ArgHomeScreen" component={ArgHomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#E8A0BF",
-  },
-  header: {
-    fontSize: 48,
-    fontWeight: "bold",
-    marginBottom: 10,
-    textAlign: "center",
-    color: "white",
-  },
-  subheading: {
-    fontSize: 30,
-    marginBottom: 20,
-    color: "white",
-  },
-  bodyText: {
-    fontSize: 18,
-    marginBottom: 20,
-    textAlign: "center",
-    color: "white",
-    marginHorizontal: 13,
-    borderColor: "red",
-  },
-  image: {
-    width: 350,
-    height: 350,
-    marginBottom: 20,
-    borderRadius: 50,
-  },
-  button: {
-    backgroundColor: "#BA90C6",
-    padding: 15,
-    width: 300,
-    borderRadius: 50,
-  },
-  buttonText: {
-    fontSize: 28,
-    textAlign: "center",
-    color: "white",
-  },
+
 });
+
+export default App;
