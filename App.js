5f072e9acba40607f76c1757b0c5e4a2c7fcb2f1
Arely Rivera <arelyrivera1996@gmail.com>
2023-06-10 10:14:59 -0700

    added home icon to nav

--- App.js
+++ App.js
@@ -1,17 +1,34 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet } from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 import HomeScreen from "./src/screens/HomeScreen.js"
 import ArgHomeScreen from "./src/screens/ArgHomeScreen.js"
+
+import HomeIcon from "./assets/nav_icons/icons-home.png"
 
 const Tab = createBottomTabNavigator();
 
 function App() {
   return (
     <NavigationContainer >
-      <Tab.Navigator initialRouteName="Home">
-        <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Navigator 
+      initialRouteName="Home"
+      screenOptions={{
+        tabBarActiveTintColor: '#e91e63',
+      }}
+      >
+        <Tab.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" size={24} color="black" />
+            ),
+          }}
+        />
         <Tab.Screen name="ArgHomeScreen" component={ArgHomeScreen} />
       </Tab.Navigator>
     </NavigationContainer>
